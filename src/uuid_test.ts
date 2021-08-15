59f2036f92779b442da91bd52989d54cf7d32bf8
Hugo Josefson <hugo@josefson.org>
2021-08-15 16:42:49 +0000

    Use deno.land/x/test_suite instead.

--- src/uuid_test.ts
+++ src/uuid_test.ts
@@ -1,6 +1,6 @@
 import { assertStrictEquals } from "https://deno.land/std@0.104.0/testing/asserts.ts";
+import { describe, it } from "https://deno.land/x/test_suite@v0.8.0/mod.ts";
 import { uuid } from "./uuid.ts";
-import { describe, it } from "./_test.ts";
 
 describe("uuid", () => {
   it("is a string", () => {
